7a8d73624ba9a276b5f79189678ba94797a86613
mobasmaji <m.basmaji@gmail.com>
2020-12-16 18:22:56 +0100

    last commit

--- api/user/route/marker.js
+++ api/user/route/marker.js
@@ -3,12 +3,12 @@ const markerRouter = express.Router();
 const markerController = require("../controller/markerController");
 const auth = require("../../../config/auth")
 
-markerRouter.post("/add", markerController.addMarker);
+markerRouter.post("/add",auth, markerController.addMarker);
 markerRouter.post("/delete", auth, markerController.deleteMarker);
-markerRouter.post("/getAll", markerController.getAll);
+markerRouter.post("/getAll",auth, markerController.getAll);
 markerRouter.post("/getShared", markerController.getShared);
-markerRouter.post("/get", markerController.getMarker);
-markerRouter.post("/update", markerController.updateMarker);
+markerRouter.post("/get",auth, markerController.getMarker);
+markerRouter.post("/update",auth, markerController.updateMarker);
 
 
 
